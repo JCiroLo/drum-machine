1d07bd91e7a80ae935d253481339d313187cf16f
JCiroLo <juanciro35@gmail.com>
2023-03-24 10:42:15 -0500

    Updated to tonejs

--- src/models/Sequencer.js
+++ src/models/Sequencer.js
@@ -1,48 +1,86 @@
+import * as Tone from 'tone'
+
+class Sample {
+  constructor (url) {
+    this.track = new Tone.Player(url).toDestination()
+    this.isMuted = false
+  }
+}
+
 export default class Sequencer {
-  cicle = null
+  samples = {
+    sfx1: new Sample(require('../assets/samples/drum_conga.wav')),
+    sfx2: new Sample(require('../assets/samples/drum_clank.wav')),
+    sfx3: new Sample(require('../assets/samples/drum_indian.wav')),
+    sfx4: new Sample(require('../assets/samples/drum_kick.wav'))
+  }
 
   constructor ({ bpm, bars }) {
     this.bpm = bpm
     this.bars = bars
     this.status = 'off'
+    this.grid = this.loadSamples()
+
+    Tone.start()
+    Tone.getDestination().volume.rampTo(-10, 0.001)
+    this.loop()
   }
 
-  play (callback) {
+  loadSamples () {
+    const { sfx1, sfx2, sfx3, sfx4 } = this.samples
+
+    return {
+      0: [sfx1, sfx2, sfx3, sfx4],
+      1: [sfx1],
+      2: [sfx1, sfx2],
+      3: [sfx1, sfx3],
+      4: [sfx1, sfx2, sfx4],
+      5: [sfx1],
+      6: [sfx1, sfx2, sfx3],
+      7: [sfx1],
+      8: [sfx1, sfx2, sfx4],
+      9: [sfx1, sfx3],
+      10: [sfx1, sfx2],
+      11: [sfx1]
+    }
+  }
+
+  loop () {
+    let beat = 0
+
+    const repeat = time => {
+      this.grid[beat].forEach(sample => {
+        if (!sample.isMuted) {
+          sample.track.start(time)
+        }
+      })
+
+      beat = (beat + 1) % (this.bars * 2)
+    }
+
+    Tone.Transport.bpm.value = this.bpm
+    Tone.Transport.scheduleRepeat(repeat, '8n')
+  }
+
+  play () {
     if (this.status === 'off') {
       this.status = 'on'
-
-      let timer = 0
-
-      const divisions = [
-        { id: 'half-bar', t: 0.5 },
-        { id: 'three-quarter-bar', t: 0.75 },
-        { id: 'one-bar', t: 1 },
-        { id: 'two-bar', t: 2 }
-      ]
-
-      this.cicle = setInterval(() => {
-        divisions.forEach(d => {
-          const beat = (60 / this.bpm) * d.t * 1000
-
-          if (timer % beat >= 0 && timer % beat < 10) {
-            callback(d)
-          }
-        })
-
-        if (timer > (60 / this.bpm) * this.bars * 1000) {
-          timer = 0
-          console.log('bar is changed')
-        }
-
-        timer += 10
-      }, 10)
+      Tone.Transport.start()
     }
   }
 
   stop () {
-    this.status = 'off'
-    clearInterval(this.cicle)
+    if (this.status === 'on') {
+      this.status = 'off'
+      Tone.Transport.stop()
+    }
   }
+
+  getSamples () {
+    return this.samples
+  }
+
+  mute () {}
 
   isPlaying () {
     return this.status === 'on'
--- src/views/Home/script.js
+++ src/views/Home/script.js
@@ -1,12 +1,12 @@
 import Sequencer from '../../models/Sequencer'
-import Sample from '../../models/Sample'
 
 export default {
   name: 'drum-machine',
   data () {
     return {
       settings: {
-        bpm: 90,
+        bpm: 180,
+        bars: 6,
         volume: {
           hat: 0.5,
           kick: 0.5,
@@ -14,46 +14,27 @@ export default {
           snare: 0.5
         }
       },
-      sequencer: new Sequencer({ bpm: 90, bars: 8 }),
-      drums: {
-        hat: new Sample({
-          url: require('../../assets/samples/drum_closed_hihat.wav')
-        }),
-        kick: new Sample({
-          url: require('../../assets/samples/drum_kick.wav')
-        }),
-        clank: new Sample({
-          url: require('../../assets/samples/drum_clank.wav')
-        }),
-        snare: new Sample({
-          url: require('../../assets/samples/drum_snare.wav')
-        })
-      }
+      sequencer: null
     }
   },
   methods: {
     power () {
+      this.sequencer = new Sequencer({
+        bpm: this.settings.bpm,
+        bars: this.settings.bars
+      })
+    },
+    play () {
       if (this.sequencer.isPlaying()) {
         this.sequencer.stop()
       } else {
-        this.sequencer.play(({ id }) => {
-          if (id === 'half-bar' && this.drums.hat.isPlaying()) {
-            this.drums.hat.play()
-          }
-          if (id === 'one-bar' && this.drums.kick.isPlaying()) {
-            this.drums.kick.play()
-          }
-          if (id === 'three-quarter-bar' && this.drums.clank.isPlaying()) {
-            this.drums.clank.play()
-          }
-          if (id === 'two-bar' && this.drums.snare.isPlaying()) {
-            this.drums.snare.play()
-          }
-        })
+        this.sequencer.play()
       }
     },
-    test () {
-      console.log('first')
+    test (a) {
+      a.mute()
+      console.log(a)
     }
-  }
+  },
+  mounted () {}
 }
